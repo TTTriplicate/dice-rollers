96e079e7ce3685dfc4605ad77ba18075b1c0ee5a
TTTriplicate <tttriplicate@gmail.com>
2022-10-01 16:13:58 -0400

    adding the ability to apply penalties as well as bonuses

--- dieRoller.py
+++ dieRoller.py
@@ -12,7 +12,7 @@ if len(sys.argv) < 2:
     print("Roller require argument: d[die_size]+[modifier]")
 
 for r in sys.argv[1:]:
-    parts = re.match(r'^([1-9])d([0-9]*)\+([0-9]*)', r)
+    parts = re.match(r'^([1-9])d([0-9]*)([+-])([0-9]*)', r)
 #    print("number of dice:", parts.group(1))
 #    print("Die size:", parts.group(2))
 #    print("Bonus:", parts.group(3))
@@ -21,4 +21,7 @@ for r in sys.argv[1:]:
     for i in range(int(parts.group(1))):
         total += (random.randint(1, int(parts.group(2))))
     print(total)
-    print(total + int(parts.group(3)))
+    if parts.group(3) == '+':
+        print(total + int(parts.group(4)))
+    else:
+        print(total - int(parts.group(4)))
